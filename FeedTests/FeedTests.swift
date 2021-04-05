241cfbb193714bbc5555be627c3ca92d1c13a6e0
ohlulu <z30262226@gmail.com>
2021-04-05 22:34:27 +0800

    [ Add ] : `<HTTPClient>` and `RemoteFeedLoader` then test load with URL should assign to client

--- FeedTests/FeedTests.swift
+++ FeedTests/FeedTests.swift
@@ -7,16 +7,43 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    
+    var client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load(url: URL) {
+        client.requestURL = url
+    }
+}
+
+protocol HTTPClient {
+    var requestURL: URL? { get set }
+}
+
 class FeedTests: XCTestCase {
 
     func test_init_clientDoesNotRequestDataFromURL() {
-        let sut = HTTPClient()
+        let sut = MockHTTPClient()
         XCTAssertNil(sut.requestURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let client = MockHTTPClient()
+        let sut = RemoteFeedLoader(client: client)
+
+        sut.load(url: url)
+
+        XCTAssertEqual(client.requestURL, url)
     }
     
     // MARK: - Helper
     
-    private class HTTPClient {
+    private class MockHTTPClient: HTTPClient {
         var requestURL: URL?
     }
 }
