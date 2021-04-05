17c6c8ce908960cfcde078593dc2e18f9c35e7bf
ohlulu <z30262226@gmail.com>
2021-04-05 22:37:48 +0800

    [ Ref ] : create a SUT factory method

--- FeedTests/FeedTests.swift
+++ FeedTests/FeedTests.swift
@@ -27,14 +27,13 @@ protocol HTTPClient {
 class FeedTests: XCTestCase {
 
     func test_init_clientDoesNotRequestDataFromURL() {
-        let sut = MockHTTPClient()
-        XCTAssertNil(sut.requestURL)
+        let (_, client) = makeSUT()
+        XCTAssertNil(client.requestURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = MockHTTPClient()
-        let sut = RemoteFeedLoader(client: client)
+        let (sut, client) = makeSUT()
 
         sut.load(url: url)
 
@@ -42,6 +41,12 @@ class FeedTests: XCTestCase {
     }
     
     // MARK: - Helper
+    
+    private func makeSUT() -> (sut: RemoteFeedLoader, client: HTTPClient) {
+        let client = MockHTTPClient()
+        let sut = RemoteFeedLoader(client: client)
+        return (sut, client)
+    }
     
     private class MockHTTPClient: HTTPClient {
         var requestURL: URL?
