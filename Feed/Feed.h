dd06e28747bb92f31ad260d365ae8acd3fb32975
ohlulu <z30262226@gmail.com>
2021-04-05 15:40:44 +0800

    Create Feed framework

---
+++ Feed/Feed.h
@@ -0,0 +1,18 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by Ohlulu on 2021/4/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
