4a1db37a33a30b4950e7d7fd041dd88e98fd95a2
ohlulu <z30262226@gmail.com>
2021-06-16 19:46:17 +0800

    [ refactor ] : decoupling `LocalFeedLoader` dependency `FeedItemEntity`

--- Feed/Feed Cache/LocalFeedLoader.swift
+++ Feed/Feed Cache/LocalFeedLoader.swift
@@ -12,7 +12,21 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItemEntity], timestamp: Date, completion: @escaping InsertionCompletion)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+public struct LocalFeedItem: Decodable, Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
 
 public final class LocalFeedLoader {
@@ -40,9 +54,16 @@ public final class LocalFeedLoader {
     }
     
     private func cache(_ items: [FeedItemEntity], with completion: @escaping (SaveResult) -> Void) {
-        store.insert(items, timestamp: currentDate()) { [weak self] error in
+        store.insert(items.toLocal(), timestamp: currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
         }
+    }
+}
+
+private extension Array where Element == FeedItemEntity {
+    
+    func toLocal() -> [LocalFeedItem] {
+        return map { LocalFeedItem(id:$0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
     }
 }
--- FeedTests/CacheFeedUseCase_Tests.swift
+++ FeedTests/CacheFeedUseCase_Tests.swift
@@ -40,11 +40,11 @@ class CacheFeedUseCase_Tests: XCTestCase {
         let timestamp = Date()
         let items = [uniqueItem(), uniqueItem()]
         let (sut, store) = makeSUT(currentDate: { timestamp })
-
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
 
-        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(localItems, timestamp)])
     }
 
     func test_save_failsOnDeletionError() {
@@ -140,7 +140,7 @@ private extension CacheFeedUseCase_Tests {
 private class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
-        case insert([FeedItemEntity], Date)
+        case insert([LocalFeedItem], Date)
     }
 
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -161,7 +161,7 @@ private class FeedStoreSpy: FeedStore {
         deletionCompletions[index](nil)
     }
 
-    func insert(_ items: [FeedItemEntity], timestamp: Date, completion: @escaping InsertionCompletion) {
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(items, timestamp))
     }
